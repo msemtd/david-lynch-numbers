ec38a31acf70574aa41df639cec83efb4a2ed3c3
Michael Erskine <michael.erskine@geoslam.com>
2020-09-13 23:01:28 +0100

    add json format and readme

---
+++ proc.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const util = require('util')
+
+// https://www.youtube.com/watch?v=W-3MP27IU-I&list=PLTPQcjlcvvXFtR0R91Gh5j9Xi8cq0oN3Y
+
+const usdrx = /^(\d+)\/(\d+)\/(\d+)$/
+/**
+ * Munge USA date to ISO date
+ *
+ * @param {string} usd - USA date format M/D/YY with year in the 2000s
+ */
+function mungeUsaDateString (usd) {
+    const da = usdrx.exec(usd)
+    if(!da) throw Error('bad line dude')
+    const gd = new Date(Date.UTC(Number(da[3])+2000, Number(da[1]) - 1, Number(da[2])))
+    return gd.toISOString().substr(0, 10)
+}
+
+readRaw('raw.txt', (err, data) => {
+    if(err) throw err
+    const s = JSON.stringify(data, null, 2)
+    fs.writeFileSync('data2.json', s)
+})
+
+function procRawLine(line) {
+    let t = line.replace(/#.*$/, '').replace(/^\s+/, '').replace(/\s+$/, '').split(/\s+/)
+    return (t.length === 2 ? [mungeUsaDateString(t[0]), Number(t[1])] : null)
+}
+
+function readRaw(f, callback) {
+    fs.readFile(f, 'utf8', (err, data) => {
+        if(err) return callback(err)
+        const lines = data.split('\n').map(line => procRawLine(line)).filter(x => x !== null)
+        return callback(null, lines)
+    })
+}
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# David Lynch Numbers #
+
+David Lynch draws numbered balls from a painted jar and presents them each day on his YouTube channel: https://www.youtube.com/channel/UCDLD_zxiuyh1IMasq9nbjrA
+
+This data is presented in `raw.txt` as raw text (with US dates) and in `data.json` as a 2D array with ISO dates.
